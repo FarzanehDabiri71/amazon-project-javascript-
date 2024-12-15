970b81184cf0b2d62870763c2230d87b523a224d
FarzanehDabiri71 <farzanehdabiri71@yahoo.com>
2024-12-15 18:15:58 +0330

    Add initial product data

---
+++ scripts/amazon.js
@@ -0,0 +1,29 @@
+const products = [
+  {
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      starts: 4.5,
+      counts: 87,
+    },
+    priceCents: 1090,
+  },
+  {
+    image: "images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      starts: 4,
+      counts: 127,
+    },
+    priceCents: 2095,
+  },
+  {
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 7,
+      counts: 56,
+    },
+    priceCents: 799,
+  },
+];
